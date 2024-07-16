0fb4c6c45cd48c8f5cb5006b0d46d81366aeca2b
Vikas <apple@apples-MacBook-Pro.local>
2024-07-16 11:27:33 +0530

    changed origon of cors

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors(
-  {
-    origin: 'http://195.35.20.31', // replace with your frontend's URL
-    credentials: true
+//   {
+//     origin: 'http://localhost:3000', // replace with your frontend's URL
+//     credentials: true   
+// }
+{
+  origin: 'http://195.35.20.31', // replace with your frontend's URL
+  credentials: true
 }
 //   {
 //   origin: 'http://localhost:3000/', // Your frontend URL
